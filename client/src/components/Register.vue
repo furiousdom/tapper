27e752c6719fd776a2255980af478972e9f1b464
Dominik Tabak <tabakdominik@gmail.com>
2020-03-28 18:42:25 +0100

    Unsuccessful styling of client register page, but functionality not broken.

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,22 +1,30 @@
 <template>
-  <div class="container">
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="Email" />
-    <br>
-    <input
-    type="password"
-    name="password"
-    v-model="password"
-    placeholder="Password" />
-    <br>
-    <div class="error" v-html="error" />
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pd-2">
+          <input
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="Email" />
+          <br>
+          <input
+            type="password"
+            name="password"
+            v-model="password"
+            placeholder="Password" />
+          <br>
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
