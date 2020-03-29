e7b2dcd535c5e8c053ff462bf9f974c0cb8dd023
Dominik Tabak <tabakdominik@gmail.com>
2020-03-30 01:56:54 +0200

    Refactored client app
    - Added Vuetify via vue-cli
    - Applied style changes

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,35 +1,56 @@
 <template>
-  <v-layout column>
-    <v-flex xs6 offset-xs3>
-      <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
-        </v-toolbar>
-        <div class="pl-4 pr-4 pt-2 pd-2">
-          <input
-            type="email"
-            name="email"
-            v-model="email"
-            placeholder="Email" />
-          <br>
-          <input
-            type="password"
-            name="password"
-            v-model="password"
-            placeholder="Password" />
-          <br>
-          <div class="error" v-html="error" />
-          <br>
-          <v-btn @click="register">Register</v-btn>
-        </div>
-      </div>
-    </v-flex>
-  </v-layout>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        fluid>
+        <v-row
+          align="center"
+          justify="center">
+          <v-col
+            cols="12"
+            sm="8"
+            md="4">
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="primary"
+                dark
+                flat>
+                <v-toolbar-title>Registration form</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Email"
+                    name="email"
+                    type="email"
+                    v-model="email" />
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    v-model="password" />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn @click="register" color="primary">Register</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import authenticationService from '../services/authenticationService.js';
+
 export default {
+  props: {
+    source: String,
+  },
   data() {
     return {
       email: '',
