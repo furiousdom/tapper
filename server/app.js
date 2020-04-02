52f5e52b9af132ddf25d3454d77833adf7143b68
Dominik Tabak <tabakdominik@gmail.com>
2020-04-02 17:58:40 +0200

    nece radi

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use('/users', require('./routes/users'));
 app.use('/orders', require('./routes/orders'));
 
-sequelize.sync()
+sequelize.sync({ force: true })
   .then(() => {
     app.listen(config.PORT, console.log(`Server started on port ${config.PORT}`));
   });
--- server/database/index.js
+++ server/database/index.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
 const config = require('../config/config');
-const db = {};
-const fs = require('fs');
-const path = require('path');
+// const db = {};
+// const fs = require('fs');
+// const path = require('path');
 
-const modelsDir = path.join(__dirname, 'models');
+// const modelsDir = path.join(__dirname, 'models');
 const sequelize = new Sequelize(
   config.db.database,
   config.db.user,
@@ -12,11 +12,25 @@ const sequelize = new Sequelize(
   config.db.options
 );
 
-fs.readdirSync(modelsDir)
-  .forEach(file => {
-    const model = sequelize.import(path.join(modelsDir, file));
-    db[model.name] = model;
-  });
+// fs.readdirSync(modelsDir)
+//   .forEach(file => {
+//     const model = sequelize.import(path.join(modelsDir, file));
+//     db[model.name] = model;
+//   });
+
+const db = {
+  Order: sequelize.import('./models/Order'),
+  OrderedProducts: sequelize.import('./models/OrderedProducts'),
+  PackageType: sequelize.import('./models/PackageType'),
+  Packaging: sequelize.import('./models/Packaging'),
+  Product: sequelize.import('./models/Product'),
+  ProductName: sequelize.import('./models/ProductName'),
+  User: sequelize.import('./models/User')
+};
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) db[modelName].associate(db);
+});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
