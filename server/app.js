ad159fb18799cf023a8c85ff173a6b83b9457565
Dominik Tabak <tabakdominik@gmail.com>
2021-01-19 23:39:46 +0100

    Added global error handler
    - Removed error handling from brand.controller as it is now handled globally
    - Removed error handling from product.controller as it is now handled globally

--- server/app.js
+++ server/app.js
@@ -12,7 +12,21 @@ app.use(cors());
 
 app.use('/', router);
 
+app.use(errorHandler);
+
+app.use((req, res, next) => res.status(404).end());
+
 sequelize.sync()
   .then(() => {
     app.listen(config.PORT, console.log(`Server started on port ${config.PORT}`));
   });
+
+function errorHandler(err, req, res, next) {
+  if (!err.status || err.status === 500) {
+    console.error(err);
+    res.status(500).end();
+    return;
+  }
+  const { status, message } = err;
+  res.status(status).json({ error: { status, message } });
+}
--- server/brand/brand.controller.js
+++ server/brand/brand.controller.js
@@ -2,42 +2,26 @@ const { Brand } = require('../shared/database');
 const status = require('http-status-codes');
 
 async function fetch(_, res) {
-  try {
-    res.send(await Brand.findAll());
-  } catch (error) {
-    res.status(status.INTERNAL_SERVER_ERROR).send({ error });
-  }
+  res.send(await Brand.findAll());
 }
 
 async function create({ body: { name, availableLiters } }, res) {
-  try {
-    res.status(status.CREATED).send(await Brand.create({
-      name,
-      availableLiters
-    }));
-  } catch (error) {
-    res.status(status.INTERNAL_SERVER_ERROR).send({ error });
-  }
+  res.status(status.CREATED).send(await Brand.create({
+    name,
+    availableLiters
+  }));
 }
 
 async function update({ params: { id }, body: { availableLiters } }, res) {
-  try {
-    const brand = await Brand.findByPk(id);
-    brand.availableLiters = availableLiters;
-    res.send(await brand.save());
-  } catch (error) {
-    res.status(status.INTERNAL_SERVER_ERROR).send({ error });
-  }
+  const brand = await Brand.findByPk(id);
+  brand.availableLiters = availableLiters;
+  res.send(await brand.save());
 }
 
 async function remove({ params: { id } }, res) {
-  try {
-    const brand = await Brand.findByPk(id);
-    const arr = await brand.destroy();
-    if (arr) res.sendStatus(status.OK);
-  } catch (error) {
-    res.status(status.INTERNAL_SERVER_ERROR).send({ error });
-  }
+  const brand = await Brand.findByPk(id);
+  const arr = await brand.destroy();
+  if (arr) res.sendStatus(status.OK);
 }
 
 async function getQuantity(req, res) {
